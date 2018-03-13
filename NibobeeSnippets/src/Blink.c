cc19de227b745fbd2cb84662a069f3abf75a772c
Markus <markusbraunwarth@gmail.com>
2018-03-13 08:30:16 +0100

    Nibobee Snippets added

---
+++ NibobeeSnippets/src/Blink.c
@@ -0,0 +1,31 @@
+/*
+ * Blink.c
+ *
+ *  Created on: 12.03.2018
+ *      Author: markus
+ */
+
+#include <nibobee/iodefs.h>
+#include <nibobee/led.h>
+#include <nibobee/delay.h>
+
+int main() {
+	// Initialisierung
+	led_init();
+
+	/*
+	 * Hier kommt der Code rein, den der Nibobee dann ausführen soll.
+	 * while(1) sorgt für eine Dauerschleife
+	 */
+	while(1) {
+		// Mache LED (rechts gelb) aus
+		led_set(LED_R_YE, 0);
+		// Mache LED (links gelb) an
+		led_set(LED_L_YE, 1);
+		delay(500);
+		led_set(LED_L_YE, 0);
+		led_set(LED_R_YE, 1);
+		delay(500);
+	}
+}
+
